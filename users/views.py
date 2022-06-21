e2adbd4631c438a5fe2e5093571ca403c952c742
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-22 01:52:58 +0300

    create view function to display and update profile.

--- users/views.py
+++ users/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .forms import *
 
 # Create your views here.
 def profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+      
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        
+        if profile_form.is_valid:
+         
+            profile_form.save()
+           
+            return redirect('dashboard/landing')
+        
+        else:
+        
+            profile_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {   
+        'current_user':current_user,
+        'profile_form' : profile_form,
+    }
     
-    return render(request, 'users/profile.html')
+    return render(request, 'users/profile.html',context)
