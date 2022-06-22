897cc9e546b21b227c6d97d6b2ac9302477af9c1
Cencious <user.email>
2022-06-22 06:48:00 +0300

    add authentication

--- pesatrack/urls.py
+++ pesatrack/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
-
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +24,5 @@ urlpatterns = [
     path('accounts/', include('users.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('register/', user_views.register, name = 'register'),
 ]
