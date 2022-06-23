92ddb07fec74341b93bf67d43a1c5caaaa456075
sirgama <sirengogamaliel@gmail.com>
2022-06-23 05:34:46 +0300

    add expenses model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,6 +1,7 @@
 from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import CharField
 
 # Create your models here.
 
@@ -9,3 +10,13 @@ class Categories(models.Model):
     
     def __str__(self):
         return self.category
+    
+class Expenses(models.Model):
+    description = models.CharField(max_length=30)
+    amount = models.IntegerField(default=0)
+    date = models.DateField(auto_now_add=True)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.description
